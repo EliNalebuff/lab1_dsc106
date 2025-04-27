f7a9e7533bfef00b4ccb2e506abb29d1e4bebbdd
EliNalebuff <enalebuff@ucsd.edu>
2025-04-26 21:39:35 -0700

    test

--- global.js
+++ global.js
@@ -105,5 +105,5 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
 }
 
 export async function fetchGitHubData(username) {
-  return fetchJSON(`https://api.github.com/users/${'elinalebuff'}`);
+  return fetchJSON(`https://api.github.com/users/${username}`);
 }
